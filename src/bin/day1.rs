36b8776065d7272969e6e9e815962abf85d7b91c
Keno Hassler <40292329+kenohassler@users.noreply.github.com>
2022-12-04 11:35:54 +0100

    days 1 & 2

---
+++ src/bin/day1.rs
@@ -0,0 +1,53 @@
+use std::fs;
+
+struct Elf(Vec<i32>);
+
+impl Elf {
+    fn new() -> Self {
+        Self(Vec::new())
+    }
+
+    fn add(&mut self, snack: i32) {
+        self.0.push(snack);
+    }
+
+    fn calories(&self) -> i32 {
+        self.0.iter().sum()
+    }
+}
+
+fn get_elves(input: &str) -> Vec<Elf> {
+    let mut elves = vec![Elf::new()];
+    for ll in input.lines() {
+        if ll.is_empty() {
+            elves.push(Elf::new());
+        } else {
+            let cur_elf = elves.last_mut().unwrap();
+            cur_elf.add(ll.parse::<i32>().unwrap())
+        }
+    }
+    elves
+}
+
+fn max_elf(elves: &[Elf]) -> i32 {
+    elves.iter().map(|elf| elf.calories()).max().unwrap()
+}
+
+fn top3_elves(elves: &[Elf]) -> i32 {
+    let mut calories: Vec<i32> = elves.iter().map(|elf| elf.calories()).collect();
+    calories.sort_unstable();
+    calories.reverse();
+    calories[0] + calories[1] + calories[2]
+}
+
+fn main() {
+    let example = fs::read_to_string("day1_example.txt").unwrap();
+    let elves = get_elves(&example);
+    println!("{}", max_elf(&elves));
+    println!("{}", top3_elves(&elves));
+
+    let day1 = fs::read_to_string("day1.txt").unwrap();
+    let elves = get_elves(&day1);
+    println!("{}", max_elf(&elves));
+    println!("{}", top3_elves(&elves));
+}
---
+++ src/bin/day2.rs
@@ -0,0 +1,102 @@
+use std::fs;
+
+#[derive(Debug, Copy, Clone)]
+enum Move {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+
+fn score(moves: &(Move, Move)) -> i32 {
+    use Move::{Paper, Rock, Scissors};
+    let score = match moves {
+        (Rock, Rock) | (Paper, Paper) | (Scissors, Scissors) => 3, /* draw */
+        (Rock, Paper) | (Paper, Scissors) | (Scissors, Rock) => 6, /* win  */
+        (Rock, Scissors) | (Paper, Rock) | (Scissors, Paper) => 0, /* loss */
+    };
+    score + moves.1 as i32
+}
+
+fn main() {
+    let score_acc = |acc, moves| acc + score(moves);
+
+    // example
+    let input = fs::read_to_string("day2_example.txt").unwrap();
+    let guide = make_guide(&input);
+    let total = guide.iter().fold(0, score_acc);
+
+    let guide2 = make_guide2(&input);
+    let total2 = guide2.iter().fold(0, score_acc);
+    println!("{}", total);
+    println!("{}", total2);
+
+    // real input
+    let input = fs::read_to_string("day2.txt").unwrap();
+    let guide = make_guide(&input);
+    let total = guide.iter().fold(0, score_acc);
+
+    let guide2 = make_guide2(&input);
+    let total2 = guide2.iter().fold(0, score_acc);
+    println!("{}", total);
+    println!("{}", total2);
+}
+
+fn make_guide(input: &str) -> Vec<(Move, Move)> {
+    use Move::{Paper, Rock, Scissors};
+    let mut guide = Vec::<(Move, Move)>::new();
+
+    for ll in input.lines() {
+        let (opp, me) = ll.split_once(' ').unwrap();
+
+        let opp_move = match opp {
+            "A" => Rock,
+            "B" => Paper,
+            "C" => Scissors,
+            _ => unreachable!(),
+        };
+        let my_move = match me {
+            "X" => Rock,
+            "Y" => Paper,
+            "Z" => Scissors,
+            _ => unreachable!(),
+        };
+
+        guide.push((opp_move, my_move));
+    }
+    guide
+}
+
+fn make_guide2(input: &str) -> Vec<(Move, Move)> {
+    use Move::{Paper, Rock, Scissors};
+    let mut guide = Vec::<(Move, Move)>::new();
+
+    for ll in input.lines() {
+        let (opp, me) = ll.split_once(' ').unwrap();
+
+        let opp_move = match opp {
+            "A" => Rock,
+            "B" => Paper,
+            "C" => Scissors,
+            _ => unreachable!(),
+        };
+        let my_move = match me {
+            "X" => match opp_move {
+                // need to lose
+                Rock => Scissors,
+                Paper => Rock,
+                Scissors => Paper,
+            },
+            "Y" => opp_move, // need draw
+            "Z" => match opp_move {
+                // need to win
+                Rock => Paper,
+                Paper => Scissors,
+                Scissors => Rock,
+            },
+            _ => unreachable!(),
+        };
+
+        guide.push((opp_move, my_move));
+    }
+    guide
+}
