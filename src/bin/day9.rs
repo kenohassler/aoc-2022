abe5f7f4eed791bbf658bc781c962443259b2366
Keno Hassler <40292329+kenohassler@users.noreply.github.com>
2022-12-11 23:29:50 +0100

    day9 mega simplified

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -6,24 +6,38 @@ struct Point {
     y: i32,
 }
 
+impl Point {
+    fn move_head(&mut self, dir: &Direction) {
+        match dir {
+            Direction::Up => self.y += 1,
+            Direction::Down => self.y -= 1,
+            Direction::Left => self.x -= 1,
+            Direction::Right => self.x += 1,
+        }
+    }
+
+    fn follow(&mut self, other: &Point) {
+        let dist_x = other.x - self.x;
+        let dist_y = other.y - self.y;
+
+        if dist_x.abs() == 2 || dist_y.abs() == 2 {
+            self.x += dist_x.signum();
+            self.y += dist_y.signum();
+        }
+    }
+}
+
 impl fmt::Display for Point {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "({}, {})", self.x, self.y)
     }
 }
 
-#[derive(Clone)]
 enum Direction {
-    // parsable directions
     Up,
     Down,
     Left,
     Right,
-    // tail-only directions
-    UpLeft,
-    UpRight,
-    DownLeft,
-    DownRight,
 }
 
 impl Direction {
@@ -57,209 +71,14 @@ impl Grid {
         let dir = Direction::from_str(dir);
 
         for _ in 0..dist {
-            let mut cur_dir = dir.clone();
-            for i in 0..self.rope.len() {
+            self.rope.first_mut().unwrap().move_head(&dir);
+            for i in 1..self.rope.len() {
                 // iterate through all pairs of knots in the rope
-                let (head, tail) = self.rope.split_at_mut(i + 1);
-                match Grid::move_point(head.last_mut().unwrap(), tail.first(), &cur_dir) {
-                    Some(new_dir) => cur_dir = new_dir,
-                    None => break,
-                }
+                let (head, tail) = self.rope.split_at_mut(i);
+                tail.first_mut().unwrap().follow(head.last().unwrap());
             }
             self.visited.insert(self.rope.last().unwrap().clone());
         }
-    }
-
-    /// Move head in the given direction and calculate the move direction for tail.
-    /// Returns None if no tail was given or no move is needed to catch up with head.
-    fn move_point(
-        head: &mut Point,
-        maybe_tail: Option<&Point>,
-        dir: &Direction,
-    ) -> Option<Direction> {
-        match dir {
-            Direction::Up => {
-                head.y += 1;
-                if let Some(tail) = maybe_tail {
-                    // tail.y is equal, 1 below, or 2 below.
-                    if tail.y + 2 == head.y {
-                        // diagonal move if tail.x != head.x
-                        if tail.x + 1 == head.x {
-                            return Some(Direction::UpRight);
-                        } else if tail.x - 1 == head.x {
-                            return Some(Direction::UpLeft);
-                        } else if tail.x == head.x {
-                            return Some(Direction::Up);
-                        } else {
-                            unreachable!("{head} -- {tail}");
-                        }
-                        // tail.y += 1;
-                        // tail.x = head.x;
-                    }
-                }
-            }
-            Direction::Down => {
-                head.y -= 1;
-                if let Some(tail) = maybe_tail {
-                    // tail.y is equal, 1 above, or 2 above.
-                    if tail.y - 2 == head.y {
-                        // diagonal move if tail.x != head.x
-                        if tail.x + 1 == head.x {
-                            return Some(Direction::DownRight);
-                        } else if tail.x - 1 == head.x {
-                            return Some(Direction::DownLeft);
-                        } else if tail.x == head.x {
-                            return Some(Direction::Down);
-                        } else {
-                            unreachable!("{head} -- {tail}");
-                        }
-                        // tail.y -= 1;
-                        // tail.x = head.x;
-                    }
-                }
-            }
-            Direction::Left => {
-                head.x -= 1;
-                if let Some(tail) = maybe_tail {
-                    // tail.x is equal, 1 to the right, or 2 to the right.
-                    if tail.x - 2 == head.x {
-                        // diagonal move if tail.y != head.y
-                        if tail.y + 1 == head.y {
-                            return Some(Direction::UpLeft);
-                        } else if tail.y - 1 == head.y {
-                            return Some(Direction::DownLeft);
-                        } else if tail.y == head.y {
-                            return Some(Direction::Left);
-                        } else {
-                            unreachable!("{head} -- {tail}");
-                        }
-                        // tail.x -= 1;
-                        // tail.y = head.y;
-                    }
-                }
-            }
-            Direction::Right => {
-                head.x += 1;
-                if let Some(tail) = maybe_tail {
-                    // tail.x is equal, 1 to the left, or 2 to the left.
-                    if tail.x + 2 == head.x {
-                        // diagonal move if tail.y != head.y
-                        if tail.y + 1 == head.y {
-                            return Some(Direction::UpRight);
-                        } else if tail.y - 1 == head.y {
-                            return Some(Direction::DownRight);
-                        } else if tail.y == head.y {
-                            return Some(Direction::Right);
-                        } else {
-                            unreachable!("{head} -- {tail}");
-                        }
-                        // tail.x += 1;
-                        // tail.y = head.y;
-                    }
-                }
-            }
-            Direction::UpLeft => {
-                head.x -= 1;
-                head.y += 1;
-                if let Some(tail) = maybe_tail {
-                    // need to move if either y is 2 away or x is 2 away
-                    if tail.y + 2 == head.y {
-                        if tail.x == head.x {
-                            // straight move
-                            return Some(Direction::Up);
-                        } else {
-                            // diagonal move
-                            return Some(Direction::UpLeft);
-                        }
-                    }
-                    if tail.x - 2 == head.x {
-                        if tail.y == head.y {
-                            // straight move
-                            return Some(Direction::Left);
-                        } else {
-                            // diagonal move
-                            return Some(Direction::UpLeft);
-                        }
-                    }
-                }
-            }
-            Direction::UpRight => {
-                head.x += 1;
-                head.y += 1;
-                if let Some(tail) = maybe_tail {
-                    // need to move if either y is 2 away or x is 2 away
-                    if tail.y + 2 == head.y {
-                        if tail.x == head.x {
-                            // straight move
-                            return Some(Direction::Up);
-                        } else {
-                            // diagonal move
-                            return Some(Direction::UpRight);
-                        }
-                    }
-                    if tail.x + 2 == head.x {
-                        if tail.y == head.y {
-                            // straight move
-                            return Some(Direction::Right);
-                        } else {
-                            // diagonal move
-                            return Some(Direction::UpRight);
-                        }
-                    }
-                }
-            }
-            Direction::DownLeft => {
-                head.x -= 1;
-                head.y -= 1;
-                if let Some(tail) = maybe_tail {
-                    // need to move if either y is 2 away or x is 2 away
-                    if tail.y - 2 == head.y {
-                        if tail.x == head.x {
-                            // straight move
-                            return Some(Direction::Down);
-                        } else {
-                            // diagonal move
-                            return Some(Direction::DownLeft);
-                        }
-                    }
-                    if tail.x - 2 == head.x {
-                        if tail.y == head.y {
-                            // straight move
-                            return Some(Direction::Left);
-                        } else {
-                            // diagonal move
-                            return Some(Direction::DownLeft);
-                        }
-                    }
-                }
-            }
-            Direction::DownRight => {
-                head.x += 1;
-                head.y -= 1;
-                if let Some(tail) = maybe_tail {
-                    // need to move if either y is 2 away or x is 2 away
-                    if tail.y - 2 == head.y {
-                        if tail.x == head.x {
-                            // straight move
-                            return Some(Direction::Down);
-                        } else {
-                            // diagonal move
-                            return Some(Direction::DownRight);
-                        }
-                    }
-                    if tail.x + 2 == head.x {
-                        if tail.y == head.y {
-                            // straight move
-                            return Some(Direction::Right);
-                        } else {
-                            // diagonal move
-                            return Some(Direction::DownRight);
-                        }
-                    }
-                }
-            }
-        }
-        None
     }
 }
 
