ad1d03d30cc96e7c5a5e9cf43e4da4b6f4f24f45
Keno Hassler <40292329+kenohassler@users.noreply.github.com>
2022-12-14 02:23:47 +0100

    error handling (anyhow)

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,5 +1,8 @@
-use std::{cell::RefCell, collections::VecDeque, fmt, ops::DerefMut};
+use std::{
+    cell::RefCell, collections::VecDeque, fmt, num::ParseIntError, ops::DerefMut, str::FromStr,
+};
 
+use anyhow::{anyhow, ensure, Context, Result};
 use itertools::Itertools;
 
 struct Item(u64);
@@ -10,11 +13,44 @@ impl fmt::Display for Item {
     }
 }
 
+enum Operand {
+    OldValue,
+    Immediate(u64),
+}
+
+impl FromStr for Operand {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "old" => Ok(Self::OldValue),
+            num => Ok(Self::Immediate(num.parse()?)),
+        }
+    }
+}
+
+enum Operator {
+    Plus,
+    Times,
+}
+
+impl FromStr for Operator {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "*" => Ok(Self::Times),
+            "+" => Ok(Self::Plus),
+            e => Err(anyhow!("unsupported operator: {e}")),
+        }
+    }
+}
+
 struct Monkey {
     items: VecDeque<Item>,
-    left: String,
-    right: String,
-    op: String,
+    left: Operand,
+    right: Operand,
+    op: Operator,
     divisor: u64,
     monkey_true: usize,
     monkey_false: usize,
@@ -22,54 +58,59 @@ struct Monkey {
 }
 
 impl Monkey {
-    fn new(input: &str, idx: usize) -> Self {
+    fn new(input: &str, idx: usize) -> Result<Self> {
         let mut lines = input.lines();
-        let mut line_helper = || lines.next().unwrap().split_once(':').unwrap();
+        let mut line_helper = || {
+            lines
+                .next()
+                .context("expected six input lines per monkey")?
+                .split_once(':')
+                .context("expected a colon in each input line")
+        };
 
         // first line: monkey ID
-        let id: usize = line_helper().0.split_once(' ').unwrap().1.parse().unwrap();
-        assert_eq!(id, idx);
+        let id: usize = line_helper()?
+            .0
+            .split_once(' ')
+            .context("no whitespace in monkey name")?
+            .1
+            .parse()?;
+        ensure!(id == idx, "expected monkeys in ascending order");
 
         // second line: items
-        let items: VecDeque<Item> = line_helper()
+        let items = line_helper()?
             .1
             .split(',')
-            .map(|num| Item(num.trim().parse().unwrap()))
-            .collect();
+            .map(|num| num.trim().parse().map(Item))
+            .collect::<Result<VecDeque<Item>, _>>()?;
 
         // third line: operation
-        let mut op_text = line_helper().1.split_ascii_whitespace();
-        assert_eq!(op_text.next().unwrap(), "new");
-        assert_eq!(op_text.next().unwrap(), "=");
-        let left = op_text.next().unwrap().to_owned();
-        let op = op_text.next().unwrap().to_owned();
-        let right = op_text.next().unwrap().to_owned();
+        let mut op_text = line_helper()?.1.split_ascii_whitespace();
+        let left = op_text.nth(2).context("missing lhs")?.parse()?;
+        let op = op_text.next().context("missing op")?.parse()?;
+        let right = op_text.next().context("missing rhs")?.parse()?;
 
         // fourth line: test
-        let mut test_text = line_helper().1.split_ascii_whitespace();
-        assert_eq!(test_text.next().unwrap(), "divisible");
-        assert_eq!(test_text.next().unwrap(), "by");
-        let divisor = test_text.next().unwrap().parse().unwrap();
+        let mut test_text = line_helper()?.1.split_ascii_whitespace();
+        let divisor = test_text.nth(2).context("missing divisor")?.parse()?;
 
         // fifth line: monkey_true
-        let monkey_true = line_helper()
+        let monkey_true = line_helper()?
             .1
             .split_ascii_whitespace()
             .last()
-            .unwrap()
-            .parse()
-            .unwrap();
+            .context("no words after the colon")?
+            .parse()?;
 
         // sixth line: monkey_false
-        let monkey_false = line_helper()
+        let monkey_false = line_helper()?
             .1
             .split_ascii_whitespace()
             .last()
-            .unwrap()
-            .parse()
-            .unwrap();
+            .context("no words after the colon")?
+            .parse()?;
 
-        Monkey {
+        Ok(Monkey {
             items,
             left,
             right,
@@ -78,22 +119,21 @@ impl Monkey {
             monkey_true,
             monkey_false,
             item_counter: 0,
-        }
+        })
     }
 
     fn operation(&self, item: &mut Item) {
-        let left = match self.left.as_str() {
-            "old" => item.0,
-            num => num.parse().expect("Expected a number"),
+        let left = match self.left {
+            Operand::OldValue => item.0,
+            Operand::Immediate(num) => num,
         };
-        let right = match self.right.as_str() {
-            "old" => item.0,
-            num => num.parse().expect("Expected a number"),
+        let right = match self.right {
+            Operand::OldValue => item.0,
+            Operand::Immediate(num) => num,
         };
-        match self.op.as_str() {
-            "*" => item.0 = left * right,
-            "+" => item.0 = left + right,
-            e => panic!("unsupported operation: {e}"),
+        match self.op {
+            Operator::Times => item.0 = left * right,
+            Operator::Plus => item.0 = left + right,
         }
     }
 
@@ -148,47 +188,49 @@ impl fmt::Display for Monkey {
     }
 }
 
-fn main() {
+fn main() -> Result<()> {
     let example = aoc_2022::example(11);
     let input = aoc_2022::input(11);
 
-    let example_monkeys = parse(&example);
+    let example_monkeys = parse(&example)?;
     let counts = do_rounds(20, &example_monkeys, true);
-    println!("{}", monkey_business(counts));
+    println!("{}", monkey_business(counts)?);
 
-    let input_monkeys = parse(&input);
+    let input_monkeys = parse(&input)?;
     let counts = do_rounds(20, &input_monkeys, true);
-    println!("{}", monkey_business(counts));
+    println!("{}", monkey_business(counts)?);
 
     // part 2
-    let example_monkeys = parse(&example);
+    let example_monkeys = parse(&example)?;
     let counts = do_rounds(20, &example_monkeys, false);
-    println!("{}", monkey_business(counts));
+    println!("{}", monkey_business(counts)?);
 
-    let example_monkeys = parse(&example);
+    let example_monkeys = parse(&example)?;
     let counts = do_rounds(10000, &example_monkeys, false);
-    println!("{}", monkey_business(counts));
+    println!("{}", monkey_business(counts)?);
 
-    let input_monkeys = parse(&input);
+    let input_monkeys = parse(&input)?;
     let counts = do_rounds(10000, &input_monkeys, false);
-    println!("{}", monkey_business(counts));
+    println!("{}", monkey_business(counts)?);
+    Ok(())
 }
 
-fn monkey_business(counts: Vec<u64>) -> u64 {
+fn monkey_business(counts: Vec<u64>) -> Result<u64> {
+    ensure!(counts.len() >= 2, "need >= 2 monkeys for monkey business");
     let mut sorted = counts.iter().sorted_unstable().rev();
     let first = sorted.next().unwrap();
     let second = sorted.next().unwrap();
 
-    first * second
+    Ok(first * second)
 }
 
-fn parse(input: &str) -> Vec<RefCell<Monkey>> {
+fn parse(input: &str) -> Result<Vec<RefCell<Monkey>>> {
     let mut monkeys = Vec::new();
     for (idx, one_input) in input.split("\n\n").enumerate() {
-        let monkey = RefCell::new(Monkey::new(one_input, idx));
+        let monkey = RefCell::new(Monkey::new(one_input, idx)?);
         monkeys.push(monkey);
     }
-    monkeys
+    Ok(monkeys)
 }
 
 /// Euclidean algorithm for gcd, used as proxy for least common multiple
@@ -206,7 +248,7 @@ fn do_rounds(rounds: usize, monkeys: &[RefCell<Monkey>], decreasing: bool) -> Ve
         .iter()
         .map(|m| m.borrow().divisor)
         .reduce(gcd)
-        .unwrap();
+        .unwrap_or(1);
     let modulus = match decreasing {
         // fun fact: the divisors are all prime, so gcd is always 1 here -.-
         true => None,
